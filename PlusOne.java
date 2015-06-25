9be6ddc5a0089701d8a41ed4e0bc59a21807194f
joana92 <joana92@users.noreply.github.com>
2015-06-25 00:58:08 -0500

    Update PlusOne.java

--- PlusOne.java
+++ PlusOne.java
@@ -2,37 +2,26 @@ public class Solution {
     public int[] plusOne(int[] digits) {
         int len, val=0;
         len=digits.length;
-        // i=len-1;
+       // i=len-1;
         int []result=new int [len+1];
         int [] rr=new int [len];
-        System.arraycopy(digits,0,result,1,digits.length)
+        System.arraycopy(digits,0,result,1,digits.length);
+        result[0]=0;
         val=result[len]+1;
         for(int i=len;i>=0;i--){
-            if ((val>=10)){
-                result[i] = val % 10;
-                val=digits[i]+1;
-                result[i]=val % 10;
-            }
+            result[i]=val % 10;
+            if(val<10)
+            break;
             else
             {
-                if(i==len-1){
-                    result[i+1]=val;
-                }
-                else
-                    result[i+1]+=digits[i];
-                break;
+                val=result[i-1]+1;
+                
             }
-            
         }
-        if(result[0]==0){
-            for(int j=1;j<len+1;j++){
-                rr[j-1]=result[j];
-            }
-            return rr;
-        }
+        if(result[0]==0)
+        {System.arraycopy(result,1,rr,0,rr.length);
+        return rr;}
         else
-            return result;
-        
+        return result;
     }
-    
-}
+    }
