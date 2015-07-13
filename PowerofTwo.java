e34f2d5b0e287b595005d7298bad764784416cbe
joana92 <joana92@users.noreply.github.com>
2015-07-12 20:45:25 -0500

    Create PowerofTwo.java

---
+++ PowerofTwo.java
@@ -0,0 +1,14 @@
+public class PowerofTwo {
+    public boolean isPowerOfTwo(int n) {
+        if(n<0) return false;
+        if(n==1) return true;
+       if(n==0) return  false;
+        while(n >= 2) {
+            if(n % 2 != 0) {
+                return false;
+            }
+            n /= 2;
+        }
+        return true;
+    }
+}
